21a924a7600978ded23b4a098fd4ab41d53178b3
anja-tadic <tadic.anja@gmail.com>
2026-03-08 00:44:23 +0100

    changed routes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 //izmedju jednog klijenta i servera postoji jedna axios instanca
 const api= axios.create({
-    baseURL: "http://127.0.0.1:8000/api",// onda samo na ovaj url dodajemo sta nam je potrebno
-});
+    baseURL: "https://internet-tehnologije-2025-vebservisieuprave2022-production.up.railway.app/api",
+});// onda samo na ovaj url dodajemo sta nam je potrebno
+
 //opcioni interceptor za token
 api.interceptors.request.use((config)=> {
     const token = localStorage.getItem('token');

f840fcb5947d25f412f0a13e47c12656f9a6f049
anja-tadic <tadic.anja@gmail.com>
2026-03-08 03:51:23 +0100

    Changed routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Pocetna from './pages/HomePage';
@@ -10,46 +9,43 @@ import RegisterPage from './pages/RegisterPage';
 import MojiZahtevi from './pages/MojiZahtevi';
 import ZakaziTerminForm from './pages/ZakaziTerminForm';
 import AdminPanel from './pages/AdminPanel';
-import { useState} from "react";
+import { useState } from "react";
 import UsersPanelAdmin from './pages/UsersPanelAdmin';
 import UserProfileAdmin from './pages/UserProfileAdmin';
 import UnprocessedRequestsPage from './pages/UnprocessedRequestsPage';
 import RequestDetailsAdmin from "./pages/RequestDetailsAdmin";
- const getUserFromStorage = () => {
-   try {
-     const raw = localStorage.getItem("user");
-     return raw ? JSON.parse(raw) : null;
-   } catch {
-     return null;
-   }
- };
 
+const getUserFromStorage = () => {
+  try {
+    const raw = localStorage.getItem("user");
+    return raw ? JSON.parse(raw) : null;
+  } catch {
+    return null;
+  }
+};
 
-//ako zelimo da NavBar bude na svakoj stranici, navodimo ga ovde
 function App() {
-  const user = getUserFromStorage();
+  const [user, setUser] = useState(getUserFromStorage);
+
   return (
     <BrowserRouter>
-    <NavBar></NavBar>
+      <NavBar user={user} />
       <Routes>
-       <Route path="/" element={<Pocetna user={user} />} />
-        <Route path="/login" element={<LogInPage setUser={user} />} />
+        <Route path="/" element={<Pocetna user={user} />} />
+        <Route path="/login" element={<LogInPage setUser={setUser} />} />
         <Route path="/userpage" element={<UserPage user={user} />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/mojizahtevi" element={<MojiZahtevi />} /> 
-         <Route path="/zakazi-termin" element={<ZakaziTerminForm user={user} />} />
+        <Route path="/mojizahtevi" element={<MojiZahtevi />} />
+        <Route path="/zakazi-termin" element={<ZakaziTerminForm user={user} />} />
         <Route path="/admin/statistika" element={<AdminPanel user={user} />} />
         <Route path="/admin/korisnici" element={<UsersPanelAdmin user={user} />} />
-        <Route path="/admin/korisnici/:id" element={<UserProfileAdmin user={user}/>} />
-        <Route path="/admin/neobradjeniZahtevi" element={<UnprocessedRequestsPage user={user}/>} />
-        <Route path="/admin/request/:id" element={<RequestDetailsAdmin user={user}/>} />
-
+        <Route path="/admin/korisnici/:id" element={<UserProfileAdmin user={user} />} />
+        <Route path="/admin/neobradjeniZahtevi" element={<UnprocessedRequestsPage user={user} />} />
+        <Route path="/admin/request/:id" element={<RequestDetailsAdmin user={user} />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
 
 export default App;
-
-
